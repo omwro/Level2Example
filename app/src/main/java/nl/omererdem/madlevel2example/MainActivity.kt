bdd10f2a68e4873ee5d546445bae51a39df68502
Omer Erdem <m.o.erdem@outlook.com>
2020-09-07 10:51:56 +0200

    #4 Init main

--- app/src/main/java/nl/omererdem/madlevel2example/MainActivity.kt
+++ app/src/main/java/nl/omererdem/madlevel2example/MainActivity.kt
@@ -2,10 +2,22 @@ package nl.omererdem.madlevel2example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import nl.omererdem.madlevel2example.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val reminders = arrayListOf<Reminder>()
+    private val reminderAdapter = ReminderAdapter(reminders)
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
+    }
+
+    private fun initViews() {
+        
     }
 }
